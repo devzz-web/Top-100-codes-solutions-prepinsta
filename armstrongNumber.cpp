59b2b023fcc99df4fa2ae46ffec6f758fa55b246
devzz-web <65944653+devzz-web@users.noreply.github.com>
2021-10-01 00:46:19 +0530

    Update armstrongNumber.cpp

--- armstrongNumber.cpp
+++ armstrongNumber.cpp
@@ -12,8 +12,12 @@
         //find sum of cubes of individual digits
         do
         {
+            digit = num % 10;
+            sum = sum + pow(digit,3);
+            num = num / 10;
             
-        }while(num != 0);
+        } 
+        while(num != 0);
         //checking for ArmStrong number
         if(sum == store)    
             cout << store << ” is an Armstrong number.”;
